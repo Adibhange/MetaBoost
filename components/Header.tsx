c940c80cdb1ab9544ba821e0742d582cb1ad99f0
Adibhange <bhangeaditya13@gmail.com>
2025-03-17 15:26:51 +0530

    feat: add header component and integrate Clerk authentication
    - Created a reusable header component for the application.
    - Integrated Clerk for user authentication and session management.

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="container mx-auto">
+      <nav className="flex items-center justify-between px-4 py-6">
+        <Link href={"/"}>
+          <Image
+            src={"/Logo.png"}
+            height={100}
+            width={1000}
+            alt="MetaBoost Logo"
+            className="h-10 w-42 md:h-14 md:w-56"
+          />
+        </Link>
+
+        <div className="flex items-center">
+          <SignedOut>
+            <SignInButton>
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "!size-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
